697250c1f2febc3fcd74f507d41cdba5d6f3d0fc
Faysal Ahmed <faysalahmed.dev@gmail.com>
2024-04-11 01:47:34 +0600

    wrapper app to clerk provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { cn } from '@/lib/utils';
+import { ClerkProvider } from '@clerk/nextjs';
 
 import '@/styles/global.css';
 
@@ -17,15 +18,17 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang='en'>
-            <body
-                className={cn(
-                    'min-h-screen bg-background font-sans antialiased',
-                    inter.variable
-                )}
-            >
-                {children}
-            </body>
-        </html>
+        <ClerkProvider>
+            <html lang='en'>
+                <body
+                    className={cn(
+                        'min-h-screen bg-background font-sans antialiased',
+                        inter.variable
+                    )}
+                >
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
     );
 }
